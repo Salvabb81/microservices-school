98cae2fc87576268c9c956194cdd081034a32151
Salvabb81 <salvabb81@users.noreply.github.com>
2020-10-30 11:28:25 +0100

    First push. Sharing project.

---
+++ microservicios-respuestas/src/main/java/com/salva/microservicios/app/respuestas/services/RespuestaService.java
@@ -0,0 +1,13 @@
+package com.salva.microservicios.app.respuestas.services;
+
+import com.salva.microservicios.app.respuestas.models.entity.Respuesta;
+
+public interface RespuestaService {
+
+	public Iterable<Respuesta> saveAll(Iterable<Respuesta> respuestas);
+	
+	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId);
+
+	public Iterable<Long> findExamenesIdsConRespuestasByAlumno(Long alumnoId);
+
+}
